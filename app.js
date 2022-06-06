ab688b61b2a94fdcc6011a15ac1277e5dd78323c
_Sydni Miles_ <_[sydnibean14@gmail.com](mailto:sydnibean14@gmail.com)_>
2022-06-06 14:59:30 -0400

    Updates to JS variables

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 
+const firstName = "Sydni";
+let lastName = "Miles";
+var age = 20;
 
 // Exercise 2
+firstName = "Sydni"; 
+lastName = "Miles";
+age = 20;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
